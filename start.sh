4fdd432c32cfe89ba1ea882bd456c24183b80ea0
Mars <Mars@localhost>
2023-03-04 11:18:54 -0500

    Add entry point start script

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+<<README
+    The sole purpose of this file is to serve as a clean entry point for all
+    the other scripts to be called (though technically build.sh is the one that
+    actually calls those other scripts)
+README
+
+bash build.sh
