ef9c765c1a6be133ec135a1c6f68de75e1fbfd10
Mars <Mars@localhost>
2023-03-04 11:21:02 -0500

    Replace backticks with curly braces

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 <<README
     The purpose of this script is to call the other scripts, thus making this a
     singularly contained application - the goal is to just be able to run
-    something like `bash my_script` to set up as much as possible and run all
+    something like {bash my_script} to set up as much as possible and run all
     the scripts required for this application to finish its task.
 README
 <<vardump
-    As the name suggests, the variable `vardump` is not actually intended to 
+    As the name suggests, the variable {vardump} is not actually intended to 
     be referenced. For the most part, at least right now, it is just satisfying
-    the requirement for the `read` command to have a destination var for STDIN
+    the requirement for the {read} command to have a destination var for STDIN
 vardump
 
 # Walk the user through setting things up that have not yet been automated 
