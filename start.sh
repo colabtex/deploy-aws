1e004cd2c0d93fca075117dba2c0b9e696976ecb
sol <sol@colabtex.com>
2023-03-05 17:01:18 -0500

    Change entry point into logic

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 <<README
     The sole purpose of this file is to serve as a clean entry point for all
-    the other scripts to be called (though technically build.sh is the one that
-    actually calls those other scripts)
+    the other scripts to be called (though technically configure-aws.sh is the 
+    one that actually calls those other scripts)
 README
 
-bash build.sh
+bash configure-aws.sh
