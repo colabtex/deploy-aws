81f4175c90fbbb5886c760a1eb3106a62cb1c7e3
sol <sol@colabtex.com>
2023-03-05 11:39:54 -0500

    Comment out to simplify script a bit

--- setup-aws.sh
+++ setup-aws.sh
@@ -4,9 +4,12 @@
     In another branch, will add command to install curl (prior to this script
     being called)
 curl
+function confirm() {
+    read -p "Press <ENTER> to confrim you're ready to proceed... "
+}
 sudo apt install curl -y
-read -p "First, make sure your AWS IAM user is enabled - then press enter" dump
-read -p "(press enter) About to curl, unzip, and install AWS" dump
+# read -p "First, make sure your AWS IAM user is enabled - then press enter" dump
+# read -p "(press enter) About to curl, unzip, and install AWS" dump
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o \
 "awscliv2.zip"
 unzip -u awscliv2.zip
@@ -14,10 +17,11 @@ sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli \
 --update
 echo "AWS Version -> "
 aws --version
+confirm
 firefox "https://d-9067ada41c.awsapps.com/start" &
 echo "Copy and paste AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY_ID, and \
 AWS_SESSION_TOKEN below (all in one text block selection), then press enter:"
-read "(press enter when ready)"
+confirm
 echo ""
 read AWS_ACCESS_KEY_ID_command
 read AWS_SECRET_ACCESS_KEY_ID_command
