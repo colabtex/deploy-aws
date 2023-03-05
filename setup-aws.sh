f4a6484e386837246d7c66745ff4b5898a36a7dd
Mars <Mars@localhost>
2023-03-04 22:19:31 -0500

    Several concise fixes

--- setup-aws.sh
+++ setup-aws.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+<<curl
+    For now, install curl in this script.
+    In another branch, will add command to install curl (prior to this script
+    being called)
+curl
+sudo apt install curl -y
+read -p "First, make sure your AWS IAM user is enabled - then press enter" dump
 read -p "(press enter) About to curl, unzip, and install AWS" dump
-curel "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip -u awscliv2.zip
 sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
 echo "AWS Version -> "
 aws --version
-google-chrome "https://d-9067ada41c.awsapps.com/start"
+firefox "https://d-9067ada41c.awsapps.com/start"
 read AWS_ACCESS_KEY_ID_command
 read AWS_SECRET_ACCESS_KEY_ID_command
 read AWS_SESSION_TOKEN_command
