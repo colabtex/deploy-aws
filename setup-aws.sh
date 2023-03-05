bd5a6a0dac546bfe7f4b338c497cc00077fad432
sol <sol@colabtex.com>
2023-03-05 14:13:21 -0500

    Try a different strategy

--- setup-aws.sh
+++ setup-aws.sh
@@ -18,17 +18,37 @@ sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli \
 echo "AWS Version -> "
 aws --version
 confirm
-echo "In firefox, go to -> https://d-9067ada41c.awsapps.com/start"
+
+newline
+firefox -P 'Another Profile' https://d-9067ada41c.awsapps.com/start
 confirm
-# echo "Copy and paste AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY_ID, and \
-# AWS_SESSION_TOKEN below (all in one text block selection), then press enter:"
-# confirm
-echo ""
-# rm -f awscliv2.zip
-# ls ./
-# echo "--- (the bulk of the scripts automation will go here) ---"
+
+newline
 read -p "Enter username (profile name): " profile
 echo "Using profile: ${profile}"
+read -p "Enter AKID: " akid
+read -p "Enter SAK: " sak
+read -p "Enter region [us-east-2]: " region
+read -p "Enter default VPC: " vpc_id
+echo "AKID: ${akid}"
+echo "SAK: ${sak}"
+echo "Region: ${region}"
+echo "VPC: ${vpc_id}"
+newline
+echo "Okay?"
+newline
+confirm
+export AWS_PROFILE=${profile}
+export AWS_ACCESS_KEY_ID=${akid}
+export AWS_SECRET_ACCESS_KEY=${sak}
+export AWS_DEFAULT_REGION=${region}
+export AWS_VPC_ID=${vpc_id}
+aws configure list
+newline
+confirm
+newline
+aws ec2 describe-vpcs --vpc-id ${vpc_id} --region us-east-2
+exit
 aws configure --profile ${profile}
 confirm
 read -p "Enter your default VPC: " vpc_id
